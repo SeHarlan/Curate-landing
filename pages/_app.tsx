d59cb44d12dae3d25a4b1b780217f2be60b82ce4
Scott Harlan <56773004+SeHarlan@users.noreply.github.com>
2023-04-18 22:04:59 -0700

    init

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,39 @@
+import useVHOverride from '@/hooks/useVHOverride'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
+import { Abril_Fatface, Lato, Montserrat_Subrayada } from 'next/font/google'
+
+const lato = Lato({
+  subsets: ["latin"],
+  weight: ["100", "300", "400", "700", "900"],
+  style: ["italic", "normal"],
+})
+
+const abril = Abril_Fatface({
+  subsets: ["latin"],
+  weight: ["400",],
+})
+
+const montserrat = Montserrat_Subrayada({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+})
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  useVHOverride()
+  return (
+    <>
+      <style jsx global>{`
+        body {          
+          --font-body: ${lato.style.fontFamily};
+          --font-title: ${montserrat.style.fontFamily};
+          --font-abril: ${abril.style.fontFamily};
+        }
+      `}</style>
+      <main>
+        <Component {...pageProps} />
+      </main>
+    </>
+  )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+const { fontFamily } = require('tailwindcss/defaultTheme')
+const plugin = require('tailwindcss/plugin')
 module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
@@ -7,12 +9,34 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        "body": ['var(--font-body)', ...fontFamily.sans],
+        "title": ['var(--font-title)', ...fontFamily.mono],
+        "abril": ['var(--font-abril)', ...fontFamily.mono]
       },
+      extend: {
+        textShadow: {
+          sm: '0 1px 2px var(--tw-shadow-color)',
+          DEFAULT: '0 2px 4px var(--tw-shadow-color)',
+          lg: '0 8px 16px var(--tw-shadow-color)',
+        },
+      },
+      height: { screen: 'calc(var(--vh) * 100)' },
+      maxHeight: { screen: 'calc(var(--vh) * 100)' },
+      minHeight: { screen: 'calc(var(--vh) * 100)' },
     },
   },
-  plugins: [],
+  plugins: [
+    require('@headlessui/tailwindcss'),
+    plugin(function({ matchUtilities, theme }) {
+      matchUtilities(
+        {
+          'text-shadow': (value) => ({
+            textShadow: value,
+          }),
+        },
+        { values: theme('textShadow') }
+      )
+    }),
+  ]
 }
